65099f2f474a319b6759f30a50b945d89e79c1fb
Rene Gulager <mail@renegulager.dk>
2018-10-26 13:57:52 +0200

    create policy for limit acces to resourses, do not work yet

--- create.sh
+++ create.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
 az login
-#  --resource-group TestSentiaRG \
+az group create --name TestSentiaRG --location "westeurope"  --resource-group TestSentiaRG --tags "envioment=test" "company=sentia"
 
-#az group create --name TestSentiaRG --location "westeurope"  --resource-group TestSentiaRG
-  ##--location "westeurope" \
 az group deployment create \
   --name SentiaDeployment \
   --template-file "./azuredeploy.json" \
   --resource-group TestSentiaRG \
   --parameters @azuredeploy.parameters.json
-   
+
+
+
+## create policyRule do not work
+az policy definition create --name 'allowed-resourcetypes' --display-name 'Allowed resource types' --description 'This policy enables you to specify the resource types that your organization can deploy.' --rules './azurepolicy.rules.json' --params './azurepolicy.parameters.json' --mode All
+az policy assignment create --name 'allowed-resourcetypes' --scope "/subscriptions/a7a9a08b-3738-4969-ad77-0dae1f98357a" --policy "allowed-resourcetypes"
